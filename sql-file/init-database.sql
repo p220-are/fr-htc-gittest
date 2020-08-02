e73b265e6fa41c2d692a99258b7b0a2db18e1337
raouf Retima <aretima@htcompass.fr>
2020-08-02 19:39:31 +0200

    supprimer des truc

--- sql-file/init-database.sql
+++ sql-file/init-database.sql
@@ -1,4 +1,8 @@
--- - Donnez le salaire moyen du département ‘RESEARCH’ 
+ 
 select round(avg (sal)) from emp
 inner join dept on emp.deptno=dept.deptno
 where  dname='RESEARCH'
+--Donner la liste des noms des musiciens et des titres des représentations auxquelles ils participent.
+select distinct nom,titre from musicien 
+
+
